0af8b3e5bcbcbe02aeb690e23300acd8714206ed
Hari Surendran <harisurendran@haris-imac.lan>
2022-02-14 07:46:36 -0600

    stateless page added

--- lib/dartpad/main.dart
+++ lib/dartpad/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
+
 class ExampleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(' Creating a Dartpad with a repository!'),
+        title: Text(' There is not state in this screen'),
       ),
       body: Center(
         child: Column(
           children: [
-            Text('gh_owner: Owner of the Github Account. In thise case is is: Jose Alba '),
-            Text('gh_repo: Name of the repo within the above account. In this case it is: flutter_code'),
-            Text('gh_path: Path to a dartpad_metadata.yaml file within the repo. In this case it is: lib/dartpad'),
-            Text('Collectivaly to make a dartpad with this repository the URL would be: '),
-            Text('dartpad.dev/embed-flutter.html?gh_owner=JoseAlba&gh_repo=flutter_code&gh_path=lib/dartpad'),
+            Text('View only screens that dot  not have state.'),
+            Text('Stays the same no matter what you do.'),
+            Text('Use this for screens that are not going to change.'),
           ],
         ),
       ),
