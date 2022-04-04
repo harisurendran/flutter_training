97e7934f4ee70d3e164d22335b8d212f59948a4c
harisurendran <harisurendran@gmail.com>
2022-04-04 13:12:34 -0500

    fixed the javascript compilation problem

--- lib/dartpad/main.dart
+++ lib/dartpad/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(home: ExampleScreen()));
+}
+
 class ExampleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(' There is not state in this screen'),
+        title: const Text(' There is no state in this screen'),
       ),
       body: Center(
         child: Column(
           children: [
-            Text('View only screens that dot  not have state.'),
-            Text('Stays the same no matter what you do.'),
-            Text('Use this for screens that are not going to change.'),
+            const Text('View only screens that dot  not have state.'),
+            const Text('Stays the same no matter what you do on the screen.'),
+            const Text('Use this for screens that are not going to change.'),
           ],
         ),
       ),
